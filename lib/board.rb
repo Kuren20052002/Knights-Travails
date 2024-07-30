aa72950d20ff185fb8b8226f053ed59d0d458518
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-07-30 21:59:42 +0700

    Complete Knight Travails

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,15 @@
+require_relative "knight"
+require "set"
+require "pry-byebug"
 class Board
   attr_accessor :board
 
   def initialize
     @board = Array.new(8) { Array.new(8) { " " } }
+  end
+
+  def place_knight(pos)
+    @knight = Knight.new(pos, self)
   end
 
   def display
@@ -14,5 +21,37 @@ class Board
       puts "\n\n"
     end
     puts "    1   2   3   4   5   6   7   8"
+  end
+
+  def knight_travails(start, goal)
+    start = pos_to_index(start)
+    goal = pos_to_index(goal)
+
+    place_knight(start)
+    queue = Queue.new
+    queue.push([start, [start]])
+
+    visited = Set.new([start])
+    until queue.empty?
+      current, path = queue.pop
+      @knight.move(current)
+
+      if current == goal
+        puts "You made it in #{path.length - 1} moves!"
+        path.each { |x, y| puts "[#{x + 1}, #{y + 1}]" }
+      end
+
+      @knight.possible_moves.each do |move|
+        next if visited.include?(move)
+
+        new_path = path + [move]
+        queue.push([move, new_path])
+        visited.add(move)
+      end
+    end
+  end
+
+  def pos_to_index(pos = [1, 1])
+    [pos[0] - 1, pos[1] - 1]
   end
 end
--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,9 @@
+require "pry-byebug"
 class Knight
-  @moveset = [[-1, -2], [-2, -1], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1]]
-  def initialize(pos)
+  def initialize(pos, board)
     @current_pos = pos
+    @board = board
+    @moveset = [[-1, -2], [-2, -1], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1]]
   end
 
   def possible_moves
@@ -9,12 +11,16 @@ class Knight
     @moveset.each do |move|
       x = @current_pos[0] + move[0]
       y = @current_pos[1] + move[1]
-      moves << [x, y] if x.between?(0..7) && y.between?(0..7)
+      moves << [x, y] if x.between?(0, 7) && y.between?(0, 7)
     end
     moves
   end
 
   def move(pos)
     @current_pos = pos
+  end
+
+  def to_s
+    "K"
   end
 end
