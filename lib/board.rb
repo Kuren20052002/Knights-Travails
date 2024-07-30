745ae96fbff1615b725d63eb124f17c8d6c5863b
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-07-30 16:48:18 +0700

    Board can display now

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,18 @@
 class Board
+  attr_accessor :board
+
+  def initialize
+    @board = Array.new(8) { Array.new(8) { " " } }
+  end
+
+  def display
+    @board.reverse.each_with_index do |row, row_idx|
+      print "#{row_idx}  "
+      row.each_with_index do |square, square_idx|
+        print "[#{square}] "
+      end
+      puts "\n\n"
+    end
+    puts "    1   2   3   4   5   6   7   8"
+  end
 end
